5dc85a2ed3968899a266644a37cf02333f27b690
manish-2425 <155496407+manish-2425@users.noreply.github.com>
2024-01-13 14:09:58 +0000

    fresh one

---
+++ a.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for Cover, I am DRAGON....ROAR">> dragon.txt
+grep -i "dragon" dragon.txt
+echo "Hello Guys, I am at Google Now"
+ls -lrth
+cat dragon.txt
