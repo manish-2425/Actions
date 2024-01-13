b52fe72b9d923c6788d2326b340e381d400dfb2a
manish-2425 <155496407+manish-2425@users.noreply.github.com>
2024-01-13 16:08:42 +0000

    artifact copying and parrellism

--- a.sh
+++ a.sh
@@ -2,6 +2,6 @@
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run for Cover, I am DRAGON....ROAR">> dragon.txt
 grep -i "dragon" dragon.txt
-echo "Hello Guys, I am at Google Now"
+echo "Hello Guys, I am at Google Now" >> manish.txt
 ls -lrth
 cat dragon.txt
