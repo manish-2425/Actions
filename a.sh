75ff13ef8abcd50da162db49dbaf0b42f5961ef1
manish-2425 <155496407+manish-2425@users.noreply.github.com>
2024-01-26 00:15:01 +0000

    test-1

--- a.sh
+++ a.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get install cowsay -y
-cowsay -f dragon "Run for Cover, I am DRAGON....ROAR">> dragon.txt
-grep -i "dragon" dragon.txt
+#sudo apt-get install cowsay -y
+#cowsay -f dragon "Run for Cover, I am DRAGON....ROAR">> dragon.txt
+#grep -i "dragon" dragon.txt
 echo "Hello Guys, Manish is at Google Now" >> manish.txt
-echo "Hello Guys, I am at Google Now"
-ls -lrth
-cat dragon.txt
+#echo "Hello Guys, I am at Google Now"
+#ls -lrth
+#cat dragon.txt
