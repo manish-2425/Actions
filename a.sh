ab87d117197ba8696da96e2a0d0412c65d7944d5
manish-2425 <155496407+manish-2425@users.noreply.github.com>
2024-01-26 00:19:47 +0000

    sudo

--- a.sh
+++ a.sh
@@ -3,7 +3,7 @@
 #cowsay -f dragon "Run for Cover, I am DRAGON....ROAR">> dragon.txt
 #grep -i "dragon" dragon.txt
 whoami
-echo "Hello Guys, Manish is at Google Now" >> manish.txt
+sudo echo "Hello Guys, Manish is at Google Now" >> manish.txt
 #echo "Hello Guys, I am at Google Now"
 #ls -lrth
 #cat dragon.txt
